fe38821535834c852a761b4d2b1871a2a4c4769c
Artem Milokhov <omichdno@gmail.com>
2022-10-11 14:10:58 +0300

    second task done

--- Задача_10/Task_10.cs
+++ Задача_10/Task_10.cs
@@ -7,10 +7,9 @@ Console.Clear();
 Console.WriteLine("Введите трехзначное число: ");
 
 int x = int.Parse(Console.ReadLine()!);
-int result = 0;
-int y = x / 10;
+int result;
 
-if (y >= 10 && y < 100)
+if (x >= 100 && x < 1000)
 {
     result = x % 100 / 10;
     Console.WriteLine($"Вторая цифра числа {x} -> {result} ");
